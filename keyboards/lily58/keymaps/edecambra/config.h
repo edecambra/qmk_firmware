7e45ed89de2618425550ef7b68e29347380c1b16
edecambra <edecambra>
2025-01-10 14:41:46 -0500

    commiting to add changes to config file

--- keyboards/lily58/keymaps/edecambra/config.h
+++ keyboards/lily58/keymaps/edecambra/config.h
@@ -22,6 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 180
+#define LOCKING_SUPPORT_ENABLE
+#define LOCKING_RESYNC_ENABLE
 
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_EFFECT_BREATHING
